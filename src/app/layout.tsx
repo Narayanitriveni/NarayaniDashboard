dc21232184fb7c793543bcdb22312cada9baf191
Narayanitriveni <narayanigandakitriveni>
2025-06-19 15:57:32 +0530

    vercel_analystics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -45,6 +46,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           {children}
+          <Analytics />
           <Toaster
             position="bottom-center"
             toastOptions={{
